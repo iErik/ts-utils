268883078f24368d2ddd156cb18de924751faa22
Erik Isidore <ericssonico@protonmail.ch>
2023-07-24 17:42:55 -0300

    Fix minutesToMs function

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ export const secondsToMs = (seconds: number): number =>
   seconds * 1000
 
 export const minuteToMs = (minutes: number): number =>
-  secondToMs(minutes * 60)
+  secondsToMs(minutes * 60)
 
 export const hourToMs = (hours: number): number =>
   minuteToMs(hours * 60)
